b2ed635a22eb242673ebb4f0d9c69ad456f591e4
Jonathan <jonathanstefanov1@gmail.com>
2021-04-06 20:50:32 +0200

    Créé la classe Character et le perso Haelterman, j'ai aussi mis dans un petit test de déplacement dans la Main Activity

--- app/src/main/java/com/example/humbug/MainActivity.kt
+++ app/src/main/java/com/example/humbug/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.humbug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val p1 = Haelterman(arrayOf<Int>(0,0))
+        p1.moveTop()
+        p1.moveLeft()
+        Log.d("pos", "x : " + p1.position[0].toString() + " y : " + p1.position[1].toString())
     }
 }
