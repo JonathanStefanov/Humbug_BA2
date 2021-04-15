db36ea3ba8465fea0b6b00670567e40e60a376ec
Nalyd1002 <82115404+Nalyd1002@users.noreply.github.com>
2021-04-15 21:39:22 +0200

    Revert "Added winactivity, loseactivity, levelactivity, updated  welcomeactivity"
    This reverts commit 0a2ac574a54ff0a967988014a77e703efb6fcd22.

--- app/src/main/java/com/example/humbug/WelcomeActivity.kt
+++ app/src/main/java/com/example/humbug/WelcomeActivity.kt
@@ -4,22 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_win.*
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-<<<<<<< Updated upstream:app/src/main/java/com/example/humbug/WelcomeActivity.kt
         val button: Button = findViewById(R.id.button_play)
         button.setOnClickListener {
             val intent = Intent(this@WelcomeActivity, MainActivity::class.java)
-=======
-        val buttonwelcome: Button = findViewById(R.id.button_play)
-        buttonwelcome.setOnClickListener {
-            val intent = Intent(this, LevelSelect::class.java)
->>>>>>> Stashed changes:app/src/main/java/com/example/view/WelcomeActivity.kt
             startActivity(intent)
         }
 
